7dad19c0886a07d00a3e0cda0b025085463a767d
pcb2002 <machat350@gmail.com>
2026-05-11 11:37:16 +0900

    lv4: 코드1 비밀번호 암호화

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -19,8 +19,8 @@ class PasswordEncoderTest {
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // when: 파라미터 순서를 (원래 비밀번호, 암호화된 비밀번호)로 변경
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
